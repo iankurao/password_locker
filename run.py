3d2478f042e53dc459d3dffa71559f329138ce5e
iankurao <kuraoian@gmail.com>
2019-05-13 09:34:47 +0300

    create a function that will save,delete,find check and display users

---
+++ run.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3.6
+from passwords import User
+import random
+from getpass import getpass
+
+def create_user(username, account,password):
+    '''
+    Function to create a new user
+    '''
+    new_user = User(username, account,password)
+    return new_user
+
+
+def save_user(user):
+    '''
+    Function to save users
+    '''
+    user.save_user()
+
+def del_user(user):
+    '''
+    Function to delete a user
+    '''
+    user.delete_user()
+
+def find_user(account):
+    '''
+    Function that finds a user by account name and returns the  user
+    '''
+    return User.find_by_account(account)
+
+def check_existing_user(account):
+    '''
+    Function that checks if a user exists with that account and returns Boolean
+    '''
+    return User.user_exists(account)
+
+def display_users():
+    '''
+    Function that returns all saved users
+    '''
+    return User.display_users()
+
